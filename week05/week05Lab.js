6c281dd65dc1fd442b180f867749a0695f24cfa2
John Maceira <jmac@J-Air.lan>
2023-05-25 08:13:35 -0400

    adding new code for labs and comments

--- week05/week05Lab.js
+++ week05/week05Lab.js
@@ -261,4 +261,41 @@ Question 7: Put it all together \n`)
  *
  * ↓ YOUR CODE HERE ↓ */
 
+class Movie{
+  constructor(title, director) {
+    this.title = title;
+    this.director = director;
+  }
+  describe() {
+    return `The movie ${this.title} was directed by ${this.director}.`;
+  }
+}
+class List {
+  constructor() {
+    this.movies = [];
+}
+addMovie(movie) {
+  this.movies.push(movie);
+}
+displayMovies() {
+  let movieDetails = "";
+ for (let i = 0; i < this.movies.length; i++) {
+  movieDetails += `${this.movies[i].title}, directed by ${this.director[i].director}\n`;
+ }
+ return movieDetails;
+}
+}
+
+
+let movie1 = new Movie("Jurassic Park", "Steven Spielberg");
+let movie2 = new Movie("How the Grinch stole Christmas", "Ron Howard");
+
+console.log(movie1.describe());
+console.log(movie2.describe());
+
+let list = new List;
+list.addMovie(movie1);
+list.addMovie(movie2);
+console.log(list.displayMovies());
+
 console.log(`-----------Finished------------`)
